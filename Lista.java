78d3bb1cf1c483b2a202724a4f7fbd2479e726b8
FNC-ninoMorisco <giov.morisco@gmail.com>
2026-02-22 20:03:28 +0000

    Consegna

--- Lista.java
+++ Lista.java
@@ -1,28 +1,68 @@
-//Implementa qui la classe Lista
 import java.util.NoSuchElementException;
 
 public class Lista<T> {
-    private Nodo<T> head;
 
-    public Lista() {
-        this.head = null;
+    Nodo<T> head;
+    
+    public Lista<T>(Nodo<T> head) {
+        head = null;
     }
 
-    public boolean isEmpty() {
-        return head == null;
+    public boolean vuota() {
+        if (head.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void aggiungiInTesta(T dato) {
-        Nodo<T> nuovoNodo = new Nodo<>(dato);
+        Nodo<T> nuovoNodo = new Nodo(dato);
         nuovoNodo.next = head;
         head = nuovoNodo;
     }
 
     public void aggiungiInCoda(T dato) {
-        Nodo<T> nuovoNodo = new Nodo<>(dato);
-        
+        Nodo <T> nuovoNodo = new Nodo(dato);
+
         if (head == null) {
             head = nuovoNodo;
-            return;
+        }
+
+        Nodo<T> curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = nuovoNodo;
+    }
+
+    public void aggiungiInPosizione(T dato, int posizione) {
+        if (posizione==0) {
+            this.aggiungiInTesta(dato);
+        } else {
+            Nodo<T> curr = head;
+            currI = 0;
+            while (currI<posizione-1 && curr.next!=null) {
+                curr = curr.next;
+                ++currI;
+            }
+            if (curr.next!=null) {
+                Nodo <T> nuovoNodo = new Nodo<>(dato);
+                nuovoNodo.next = curr.next;
+                curr.next = nuovoNodo;
+            } else {
+                throw new IndexOutOfBoundsException("Posizione oltre la Lista");
+            }
+            
         }
         
+    }
+
+    public T leggiTesta() {
+        if (head == null) {
+            throw new NoSuchElementException("Lista vuota");
+        }
+        return head.dato;
+    }
+
+}
